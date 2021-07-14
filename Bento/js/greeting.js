6f625e477b5f2226382a570500ecc72506213450
Saliba-san <65911528+Saliba-san@users.noreply.github.com>
2021-07-14 15:48:03 -0300

    Add files via upload

---
+++ Bento/js/greeting.js
@@ -0,0 +1,23 @@
+// Get the hour
+const today = new Date();
+const hour = today.getHours();
+
+// Here you can change your name
+const name = ' Saliba-san';
+
+// Here you can change your greetings
+const gree1 = 'Hora de capotar,';
+const gree2 = 'Bom dia!  ';
+const gree3 = 'Boa tarde,  ';
+const gree4 = 'Boa noite,  ';
+
+// Define the hours of the greetings
+if (hour >= 23 && hour < 5) {
+  document.getElementById('greetings').innerText = gree1 + name;
+} else if (hour >= 6 && hour < 12) {
+  document.getElementById('greetings').innerText = gree2 + name;
+} else if (hour >= 12 && hour < 17) {
+  document.getElementById('greetings').innerText = gree3 + name;
+} else {
+  document.getElementById('greetings').innerText = gree4 + name;
+}
